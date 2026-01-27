4529026e0955b3bb9a9dd48332be1f6331a3b2a2
Michael Shahin <shahinmg@ku.edu>
2026-01-27 13:05:40 -0600

    py files for empirical and HITL approaches

---
+++ HITL.py
@@ -0,0 +1,6 @@
+"""
+A human in the loop approach to automatically identify grounding points from xOPR
+
+"""
+
+
---
+++ empirical.py
@@ -0,0 +1,5 @@
+"""
+An empirical approach to automatically identify grounding points from xOPR accessed radar data.
+This is an xOPR approach to the methods described in Xia et al., 2025 https://ieeexplore.ieee.org/document/11202578/
+
+"""
