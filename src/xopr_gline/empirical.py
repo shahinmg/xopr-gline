c40a7ef9a3da658a7761e439766fcc88ae865af6
Michael Shahin <shahinmg@ku.edu>
2026-01-29 12:13:23 -0600

    defined hab function

--- src/xopr_gline/empirical.py
+++ src/xopr_gline/empirical.py
@@ -3,3 +3,24 @@ An empirical approach to automatically identify grounding points from xOPR acces
 This is an xOPR approach to the methods described in Xia et al., 2025 https://ieeexplore.ieee.org/document/11202578/
 
 """
+import numpy as np
+import xarray as xr
+
+
+def hab(ds: xr.Dataset) -> xr.Dataset:
+    """
+    Add height above buoyancy and thickness.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        xOPR Dataset with surface and bottom variables.
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset with height above buoyancy and thickness added in meters.
+
+    """
+    
+    return ds
