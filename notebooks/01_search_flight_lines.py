6cacf98017ed58a451ce684e9227f5930122bc02
Wei Ji <23487320+weiji14@users.noreply.github.com>
2026-02-03 16:26:58 +1300

    Script to find radar flight lines per glacier termini and cache their id
    Essentially a for-loop that goes over each glacier, does an intersection search against the STAC catalog and caches the STAC item ids per glacier to a GeoJSON file. GeoJSON is saved with columns stac_item_id, GlacierID, collection and geometry, where geometry is to 7 decimal digits according to RFC 7946.

---
+++ notebooks/01_search_flight_lines.py
@@ -0,0 +1,53 @@
+"""
+Script to loop over 200+ Greenland glaciers termini, and use the line geometries to find
+intersecting OPR flight lines via a STAC query.
+
+Results are saved to a GeoJSON file with the following table structure:
+
+|     stac_item_id     | GlacierID |     collection    |    geometry     |
+|----------------------|-----------|-------------------|-----------------|
+| Data_20020531_06_001 |     1     | 2002_Greenland_P3 | LINESTRING(...) |
+"""
+
+import geopandas as gpd
+import tqdm
+import xopr
+
+import xopr_gline
+
+# %%
+# Set up connection
+opr = xopr.OPRConnection(
+    collection_url="https://data.cresis.ku.edu/data/",
+    stac_parquet_href="gs://opr_stac/catalog/**/*.parquet",
+)
+
+# Get termini positions
+gdf_termini: gpd.GeoDataFrame = xopr_gline.geospatial.get_greenland_termini(
+    end_year=2021
+)
+assert len(gdf_termini) == 239
+
+# %%
+# Loop through glaciers termini, save out stac_item_id
+for row in tqdm.tqdm(iterable=gdf_termini.itertuples(), total=len(gdf_termini)):
+    if row.Index > 10:
+        break
+    print("\n")
+    print(f"Looking at GlacierID {row.Index}:", row.Official_n)
+    # Search for radar frames intersecting termini linestring geometry
+    stac_items: gpd.GeoDataFrame = opr.query_frames(geometry=row.geometry)
+    if stac_items is not None:
+        print(f"Found {len(stac_items)} rows")
+
+        # Save stac_item_id per GlacierID to GeoJSON file
+        stac_items["GlacierID"] = row.Index  # add GlacierID to every row
+        stac_items[["GlacierID", "collection", "geometry"]].to_file(
+            filename := "data/glacier_to_radarframe_ids.geojson",
+            RFC7946=True,  # https://gdal.org/en/stable/drivers/vector/geojson.html#rfc-7946-write-support
+            mode="w" if row.Index == 1 else "a",
+        )
+    else:
+        print("Found 0 rows")
+
+print(f"Indexes saved to {filename}")
